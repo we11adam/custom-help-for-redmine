b9b03d754db05d74a4c0ce687e20211636f1b435
Adam Lau <skyros@gmail.com>
2013-05-23 19:38:37 +0800

    Init

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :custom_help do
+  name 'Custom Help plugin'
+  author 'Adam Lau'
+  description 'This plugin allows your to change the help link as you wish.'
+  version '0.0.1'
+  url 'http://tinycode.me'
+  author_url 'http://tinycode.me'
+end
